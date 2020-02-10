6613720bf35eb90f96979a9e0e78223e23a1f38e
Baraketiseifeddine <baraketiseifeddine@gmail.com>
2020-02-10 17:45:13 +0100

    a

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry block
+ * @void: no argument
+ * Return: 0 (Sucess)
+**/
+int main(void)
+{
+int i;
+for (i = 97; i < 123; i++)
+putchar (i);
+putchar('\n');
+return (0);
+}
