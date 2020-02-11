c3bd9b927000b22412627f6be049623591a4c09c
Baraketiseifeddine <baraketiseifeddine@gmail.com>
2020-02-11 10:09:34 +0100

    a

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/** 
+ * main - entry block
+ * @void: no argument
+ * Return: 0 (Sucess)
+**/
+int main(void)
+{
+int i;
+for (i = 'a', i <= 'z', i++)
+{
+if (i != 'e' && i =! 'q')
+putchar (i);
+}
+return(0);
+}
