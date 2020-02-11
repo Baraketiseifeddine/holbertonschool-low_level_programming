4e7e77c57e7356a85ebf971a521ffcddaeee437d
Baraketiseifeddine <baraketiseifeddine@gmail.com>
2020-02-11 15:20:09 +0100

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,13 @@
 **/
 void print_alphabet_x10(void)
 {
-
-  char j, i;
-  for (i = 0; i < 10; i++)
-    {
-    for (j = 'a'; j <= 'z'; j++)
-      {
-      _putchar (j);
-      
-      }
-    _putchar ('\n');
-      }
-    }
+char j, i;
+for (i = 0; i < 10; i++)
+{
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar (j);   
+}
+_putchar ('\n');
+}
+}
