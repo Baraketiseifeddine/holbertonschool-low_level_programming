e6695232c7db9e5b65d0385812768ec67b148682
Baraketiseifeddine <baraketiseifeddine@gmail.com>
2020-02-11 10:29:41 +0100

    a

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry block
+ * @void: no argument
+ * Return: 0 (Sucess)
+**/
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+putchar (i + '0');
+putchar ('\n');
+return (0);
+}
