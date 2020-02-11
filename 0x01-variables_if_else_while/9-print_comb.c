3f93fdf1689c0b9706cd3a7fbe5e0c68948f0b37
Baraketiseifeddine <baraketiseifeddine@gmail.com>
2020-02-11 11:11:15 +0100

    a

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry block
+ * @void: no argument
+ * Return: 0 (Sucess)
+**/
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+putchar (i + '0');
+putchar (',');
+putchar (' ');
+}
+putchar ('\n');
+return (0);
+}
