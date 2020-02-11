ea431f2259d0d194f15ac67a1a5a5cceaa22ae2e
Baraketiseifeddine <baraketiseifeddine@gmail.com>
2020-02-11 21:50:28 +0100

    a

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,16 +8,16 @@
 int print_last_digit(int n)
 {
 int x, z;
-if (n > 0)
+if (n >= 0)
 {
-x = n % 10;
+x = (n % 10);
 _putchar (x + '0');
 return (x);
 }
 else
 {
 z = (n * (-1));
-x = z % 10;
+x = (z % 10);
 _putchar (x + '0');
 return (x);
 }}
