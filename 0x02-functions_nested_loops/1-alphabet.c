f7a2d7f1501a9767407f80caeb1675a18c2a0b84
Baraketiseifeddine <baraketiseifeddine@gmail.com>
2020-02-11 14:44:59 +0100

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * main - entry block
+ * @void: no argument
+ * Return: 0 (Sucess)
+**/
+int main(void)
+{
+int i;
+for (i = 'a'; i <= 'z'; i++)
+_putchar (i);
+return (0);
+}
