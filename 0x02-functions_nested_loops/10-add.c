1e02fab45c7afbcf869fb3c46125254c53bf34ed
Baraketiseifeddine <baraketiseifeddine@gmail.com>
2020-02-11 22:04:38 +0100

    a

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * add - use only user defined variables
+ * @a: is variable
+ * @b: is variable
+ * Return: 0
+**/
+int add(int a, int b)
+{
+int s;
+s = a + b;
+_putchar (s + '0');
+return (s);
+}
