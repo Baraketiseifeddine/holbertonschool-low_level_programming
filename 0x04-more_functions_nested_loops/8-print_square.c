f57cb35b47d29b2a5a804a59e305a8ac430a87aa
Baraketiseifeddine <baraketiseifeddine@gmail.com>
2020-02-13 14:40:53 +0100

    a

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_square - use only user defined variables
+ * @size: variable
+ *
+ * Return: 0
+**/
+void print_square(int size)
+{
+int i, j;
+
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+_putchar ('#');
+_putchar ('\n');
+}}
