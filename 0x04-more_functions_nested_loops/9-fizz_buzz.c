9b37f505535a69d45379e477c03739505d65b622
Baraketiseifeddine <baraketiseifeddine@gmail.com>
2020-02-13 15:52:59 +0100

    a

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 /**
- * print_numbers - use only user defined variables
+ * main - use only user defined variables
  *
  *
  * Return: 0
 **/
 int main(void)
 {
-  int i;
-  for (i = 1; i <= 100; i++)
-    {
-          if (i % 3 == 0 && i % 5 == 0)
-    printf ("FizzBuzz");
-	  else if (i % 3 == 0)  
-      printf ("Fizz");
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+printf("FizzBuzz");
+else if (i % 3 == 0)
+printf("Fizz");
 else if (i % 5 == 0)
-	  printf ("Buzz");
-
-  else
-  printf ("%d", i);
-	  if (i < 100)
-	    putchar (' ');
-    } 
-  putchar ('\n');
+printf("Buzz");
+else
+printf("%d", i);
+if (i < 100)
+putchar (' ');
+}
+putchar ('\n');
 }
