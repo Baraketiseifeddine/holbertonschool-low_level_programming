45363e3d5ed409e0160b4f53539a6cd0de3f8da8
Baraketiseifeddine <baraketiseifeddine@gmail.com>
2020-02-18 12:14:00 +0100

    a

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,11 +7,11 @@
  */
 void rev_string(char *s)
 {
-int i, j = 0, len = 0,k = 0, a;
+int i, j = 0, len = 0, k = 0, a;
 for (i = 0; s[i] != '\0'; i++)
 len = i + 1;
 for (j = len - 1; j >= len / 2 ; j--)
-{ 
+{
 a = s[k];
 s[k] = s[j];
 s[j] = a;
