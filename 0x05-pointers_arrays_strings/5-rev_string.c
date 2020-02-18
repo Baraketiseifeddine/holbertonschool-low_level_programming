1f8b167a69b838d0f039d696b1d1cef491554d4a
Baraketiseifeddine <baraketiseifeddine@gmail.com>
2020-02-18 12:12:32 +0100

    a

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <string.h>
+/**
+ * rev_string - pointers to 98
+ * @s: chaine
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+int i, j = 0, len = 0,k = 0, a;
+for (i = 0; s[i] != '\0'; i++)
+len = i + 1;
+for (j = len - 1; j >= len / 2 ; j--)
+{ 
+a = s[k];
+s[k] = s[j];
+s[j] = a;
+k++;
+}
+}
