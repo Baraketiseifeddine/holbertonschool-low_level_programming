21592e29b890d4c9ca53a61f6c08c5c924983968
Baraketiseifeddine <baraketiseifeddine@gmail.com>
2020-02-18 13:17:49 +0100

    a

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,9 +10,17 @@ void puts_half(char *str)
 int i, n, len;
 for (i = 0; str[i] != '\0'; i++)
 len = i + 1;
+if (len % 2 != 0)
 for (n = (len - 1) / 2; str[n] != '\0'; n++)
 {
-_putchar (str[n+1]);
+_putchar (str[n]);
+}
+else
+{
+for (n = len / 2; str[n] != '\0'; n++)
+{
+_putchar (str[n]);
+}
 }
 _putchar ('\n');
 }
