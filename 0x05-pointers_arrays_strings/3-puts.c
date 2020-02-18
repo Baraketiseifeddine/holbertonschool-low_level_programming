f8e0d2ddbab676dc18994e972ebced027ebfb30a
Baraketiseifeddine <baraketiseifeddine@gmail.com>
2020-02-18 10:39:24 +0100

    a

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,10 +3,13 @@
 #include <stdio.h>
 /**
  * _puts - pointers to 98
- *
+ * @str : chaine
  * Return: Always 0.
  */
 void _puts(char *str)
 {
-puts(str);
+int i;
+for (i = 0; str[i] != '\0'; i++)
+_putchar (str[i]);
+_putchar ('\n');
 }
