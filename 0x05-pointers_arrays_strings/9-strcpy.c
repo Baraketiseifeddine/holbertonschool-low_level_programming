61ea22235cbd95a8f65dc1b5b6499073ae09c698
Baraketiseifeddine <baraketiseifeddine@gmail.com>
2020-02-18 14:17:52 +0100

    a

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <string.h>
+/**
+ * *_strcpy - pointers to 98
+ * @dest: array
+ * @src: index
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i, len;
+for (i = 0; src[i] != '\0'; i++)
+len = i + 1;
+for (i = 0; i < len; i++)
+dest[i] = src[i];
+return (dest);
+}
