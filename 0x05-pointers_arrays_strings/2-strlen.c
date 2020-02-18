99835cedb6b227cf69d96da252d0946e6633befa
Baraketiseifeddine <baraketiseifeddine@gmail.com>
2020-02-18 10:34:41 +0100

    a

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,13 +2,13 @@
 #include <string.h>
 /**
  * _strlen - pointers to 98
- *
+ *@s : variable
  * Return: Always 0.
  */
 int _strlen(char *s)
 {
-int len;
-len = strlen (s);
+int i, len;
+for (i = 0; s[i] != '\0'; i++)
+len = i + 1;
 return (len);
 }
-
