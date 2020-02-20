9d9b6df65e58d6d35f1d58496e0c12e9a7a5ac0d
Baraketiseifeddine <baraketiseifeddine@gmail.com>
2020-02-20 13:58:26 +0100

    a

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,28 +1,26 @@
 #include "holberton.h"
 /**
  * _strcmp - check the code for Holberton School students.
- * @dest: variable
- * @src: variable
- * @n: variable
+ * @s1: variable
+ * @s2: variable
  * Return: Always 0.
  */
 int _strcmp(char *s1, char *s2)
 {
-int i, j, len1, len2;
+int i, j, res;
+j = 0;
 for (i = 0; s1[i] != '\0'; i++)
-len1 = i + 1;
-for (j = 0; s2[j] != '\0'; j++)
-len2 = j + 1;
+{
 if (s1[i] == s2[j])
 {
-return (len1 + len2);
-}
-else if (len1 < len2)
-{
-return (len2);
+res = s1[i] - s2[j];
+j++;
 }
 else
 {
-return (0);
+res = s1[i] - s2[j];
+return (res);
 }
+}
+return (res);
 }
