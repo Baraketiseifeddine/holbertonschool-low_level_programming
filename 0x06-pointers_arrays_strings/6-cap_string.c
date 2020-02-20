cfedd4804c30c7bcc4d24ca5e429ac19b1e9ac86
Baraketiseifeddine <baraketiseifeddine@gmail.com>
2020-02-20 18:37:39 +0100

    aa

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * *cap_string - check the code for Holberton School students.
+ * @s1: variable
+ * Return: Always 0.
+ */
+char *cap_string(char *s1)
+{
+int c[] = {32, 9, 10, 46, 44, 59, 63, 33, 34, 40, 41, 123, 125};
+int i = 0, k;
+for (i = 0; s1[i] != '\0'; i++)
+{
+for (k = 0; k < 11; k++)
+if (s1[i] >= 97 && s1[i] <= 122 && s1[i - 1] == c[k])
+s1[i] = s1[i] - 32;
+}
+return (s1);
+}
