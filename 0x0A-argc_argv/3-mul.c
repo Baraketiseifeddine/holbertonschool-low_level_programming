0ec55e0f3ff7a95de27c4e024f2d4d010bff8396
Baraketiseifeddine <baraketiseifeddine@gmail.com>
2020-02-29 19:23:08 +0100

    fix also

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - check the code for Holberton School students.
  * @argc: NUMB des arguments
- * @argv; tableau des arguements
+ * @argv: tableau des arguements
  * Return: Always 0.
  */
 int main(int argc, char **argv)
 {
 int i, result;
-for (i=0; i < argc; i++)
-if (i <= 2)
+for (i = 0; i < argc; i++)
+;
+if (i == 3)
 {
-result = argv[1] * argv[2];
-printf ("%d\n", result);
+result = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", result);
 }
 else
 {
- printf (Error, "\n");
+printf("Error\n");
+return (1);
 }
 
 }
